c541bf4c22f081ef4f00fb6bd113f6983c290471
tarang <tarang@stackfusion.io>
2024-11-06 20:52:33 +0530

    business logic

---
+++ 03-investment-app /01-starting-project/src/components/Results.jsx
@@ -0,0 +1,16 @@
+
+import { calculateInvestmentResults } from '../util/investment';
+
+export default function({input }) {
+
+    console.log(input)
+
+
+    const resultsData = calculateInvestmentResults(input)
+    return <>
+        <p>hellp</p>
+    </>
+
+
+}
+
--- 03-investment-app /01-starting-project/src/components/UserInput.jsx
+++ 03-investment-app /01-starting-project/src/components/UserInput.jsx
@@ -1,26 +1,8 @@
 
-import { useState } from "react"
+
+export default function UserInput({onChangeInput, userInput}) {
 
 
-export default function UserInput() {
-    const [userInput , setUserInput] = useState({
-        initialInvestment : 10000,
-        annualInvestment : 1200,
-        expectedReturn : 6,
-        duration : 10
-    })
-    
-    
-
-    function handleChange(inputIdentifier , newValue){
-        setUserInput(prevUserInput => {
-            return {
-                ...prevUserInput,
-                [inputIdentifier]:newValue
-            }
-        });
-        
-    }
 
 
     return <section id="user-input">
@@ -31,7 +13,7 @@ export default function UserInput() {
                 value={userInput.initialInvestment} 
                 required 
                 onChange={(event) => 
-                    { handleChange('initialInvestment' , event.target.value) } 
+                    { onChangeInput('initialInvestment' , event.target.value) } 
                 }/>
 
             </p>
@@ -41,7 +23,7 @@ export default function UserInput() {
                 value={userInput.annualInvestment} 
                 required 
                 onChange={(event) => 
-                    { handleChange('annualInvestment' , event.target.value) } 
+                    { onChangeInput('annualInvestment' , event.target.value) } 
                 }/>
             </p>
 
@@ -53,7 +35,7 @@ export default function UserInput() {
                 value={userInput.expectedReturn} 
                 required 
                 onChange={(event) => 
-                    { handleChange('expectedReturn' , event.target.value) } 
+                    { onChangeInput('expectedReturn' , event.target.value) } 
                 }/>
             </p>
             <p>
@@ -62,7 +44,7 @@ export default function UserInput() {
                 value={userInput.duration} 
                 required 
                 onChange={(event) => 
-                    { handleChange('duration' , event.target.value) } 
+                    { onChangeInput('duration' , event.target.value) } 
                 }/>
             </p>
 
