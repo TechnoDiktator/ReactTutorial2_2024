f93881b157d76d794e981aca5598097693b30f51
tarang <tarang@stackfusion.io>
2024-11-06 21:28:40 +0530

    calculations and table

--- 03-investment-app /01-starting-project/src/App.jsx
+++ 03-investment-app /01-starting-project/src/App.jsx
@@ -17,9 +17,22 @@ function App() {
     //this is the proper way of updating state that is using a function
     //inside the stateFuction
     setUserInput(prevUserInput => {
+        
+      
+        //very imp 
+        /*
+        even if you set the input type to number in an input field in 
+        the front end componenet 
+        JS wwill always receive it as a string
+        and math operations will give wierd answers for 
+        strings
+        thus we have to explicitely cxast the value to a number
+        so a shortcut to do this is to use a + sign
+        */
         return {
             ...prevUserInput,
-            [inputIdentifier]:newValue
+            [inputIdentifier]:+newValue
+                            //the added + sign converts the value into a number
         }
     });
     
--- 03-investment-app /01-starting-project/src/components/Results.jsx
+++ 03-investment-app /01-starting-project/src/components/Results.jsx
@@ -1,14 +1,47 @@
 
 import { calculateInvestmentResults } from '../util/investment';
-
-export default function({input }) {
-
+import { formatter } from '../util/investment';
+export default function Results({input }) {
+    const resultsData = calculateInvestmentResults(input)
+    const initialInvestment = (
+    resultsData[0].valueEndOfYear - 
+    resultsData[0].interest  - 
+    resultsData[0].annualInvestment
+    )
+    
     console.log(input)
 
-
-    const resultsData = calculateInvestmentResults(input)
+    console.log(resultsData)
     return <>
-        <p>hellp</p>
+        <table id='result'>
+            <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>Investment Year</th>
+                    <th>Interest(Year)</th>
+                    <th>Total Interest</th>
+                    <th>Invested Capital</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {resultsData.map((yearData) => {
+
+
+                    const totalInterestAccrossAllYears = yearData.valueEndOfYear - yearData.annualInvestment*yearData.year - initialInvestment
+                    const totalAmountInvested =  yearData.valueEndOfYear - totalInterestAccrossAllYears
+                    return <tr key={yearData.year}>
+                        <td>{yearData.year}</td>
+                        <td>{formatter.format( yearData.valueEndOfYear )}</td>
+                        <td>{formatter.format(yearData.interest)}</td>
+                        <td>{formatter.format(totalInterestAccrossAllYears)}</td>
+                        <td>{formatter.format(totalAmountInvested)}</td>
+
+                    </tr>
+
+                } )}
+            </tbody>
+        </table>
     </>
 
 
