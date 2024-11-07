10cf5d1452c98fe280bc0fb29636ef3505d020dd
tarang <tarang@stackfusion.io>
2024-11-08 00:09:29 +0530

    building all components for the project management app

--- 05-allthingslearnedtillnowproject/01-starting-project (1)/01-starting-project/src/App.jsx
+++ 05-allthingslearnedtillnowproject/01-starting-project (1)/01-starting-project/src/App.jsx
@@ -1,8 +1,47 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectSidebar from "./components/ProjectSidebar";
+
 function App() {
+
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId:undefined,
+    projects:[]
+  })
+
+
+
+  function handleStartAtProject() {
+    setProjectsState(prevState => {
+      return  {
+        ...prevState,
+        selectedProjectId:null,
+
+      }
+    });
+
+  }
+
+  let content;
+  if(projectsState.selectedProjectId === null){
+      content = <NewProject></NewProject>
+  }else if(projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAtProject}/>
+  }else{
+    ""
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
+
+      <ProjectSidebar onStartAddProject = {handleStartAtProject}></ProjectSidebar>
+      {/* <NewProject></NewProject>
+      <NewProject></NewProject>
+      <NewProject></NewProject> */}
+      {content}
+    </main>
   );
 }
 
