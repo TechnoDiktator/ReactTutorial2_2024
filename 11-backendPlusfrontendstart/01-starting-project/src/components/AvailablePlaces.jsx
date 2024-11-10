ab600a1879fe6677ed41f1b7846b273e449f3206
tarang <tarang@stackfusion.io>
2024-11-11 00:15:29 +0530

    component definition

--- 11-backendPlusfrontendstart/01-starting-project/src/components/AvailablePlaces.jsx
+++ 11-backendPlusfrontendstart/01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,10 +1,71 @@
 import Places from './Places.jsx';
+import { useState , useEffect } from 'react';
+import Error from './Error.jsx';
+
+
+
+//how to use the async await IN REACT
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  //Fetch the data from the dummy api
+  
+  const [availablePlaces ,  setAvailablePlaces] =  useState([])
+  const [isFetching , setIsFetching] = useState(true)
+  const [error , setError] = useState(null);
+  //if we did not use useeffect we would get 
+  //an infinite loop!!!!!
+  useEffect( ()=>{
+
+    //GRANDPAA APPROACH
+    async function fetchPlaces(){
+      setError(null)
+      setIsFetching(true)
+      
+      try{
+        const response = await fetch('http://localhost:3000/places');
+        if(!response.ok){
+          throw new Error("Failed to fecth data")
+        }
+        const resData =  await response.json()
+        setAvailablePlaces(resData.places)
+        if (resData){
+          setIsFetching(false)
+        }
+
+      }catch (error){
+        //...
+        setError(error)
+      }
+      setIsFetching(false)
+    }
+
+    fetchPlaces()
+    //we could use the async await syntax BUT
+    //react cmponents CANNOT be made ASYNC
+    //so this is the LONG APPROACH
+    // console.log("fetching places via dummy get api")
+
+    // fetch('http://localhost:3000/places')
+    // .then((response) => {
+    //   return response.json()
+    // })
+    // .then((responseData)=>{
+    //   console.log("data" , responseData)
+    //   setAvailablePlaces(responseData.places)
+    //  setIsFetching(true)
+    // })
+    
+  }, [])
+
+  if(error){
+    return <Error message={error.message} title="an error occured"></Error>
+  }
   return (
     <Places
       title="Available Places"
-      places={[]}
+      isLoading = {isFetching}
+      places={availablePlaces}
+      loadingText = "Fetching Data Please Wait ..... "
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
--- 11-backendPlusfrontendstart/01-starting-project/src/components/Places.jsx
+++ 11-backendPlusfrontendstart/01-starting-project/src/components/Places.jsx
@@ -1,18 +1,34 @@
-export default function Places({ title, places, fallbackText, onSelectPlace }) {
+export default function Places({ title, 
+  places, 
+  fallbackText, 
+  onSelectPlace ,
+  isLoading,
+  loadingText
+}) {
   console.log(places);
   return (
     <section className="places-category">
       <h2>{title}</h2>
-      {places.length === 0 && <p className="fallback-text">{fallbackText}</p>}
-      {places.length > 0 && (
+      
+      {/* IF LOADING fetching API */}
+      {isLoading && <p>{loadingText}</p>}
+      
+      {/* is API data result is null */}
+      {!isLoading && places.length === 0 && <p className="fallback-text">{fallbackText}</p>}
+      
+      {/* If data is present */}
+      {!isLoading && places.length > 0 && (
         <ul className="places">
           {places.map((place) => (
+            <>
+            {console.log(place)}
             <li key={place.id} className="place-item">
               <button onClick={() => onSelectPlace(place)}>
                 <img src={`http://localhost:3000/${place.image.src}`} alt={place.image.alt} />
                 <h3>{place.title}</h3>
               </button>
             </li>
+            </>
           ))}
         </ul>
       )}
