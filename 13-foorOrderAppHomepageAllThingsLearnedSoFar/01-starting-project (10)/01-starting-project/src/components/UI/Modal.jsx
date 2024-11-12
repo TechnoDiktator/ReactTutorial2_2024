bd09f542037baaba46b8c4ca04056f7b1c7db0c8
tarang <tarang@stackfusion.io>
2024-11-12 10:32:16 +0530

    context used for modal

--- 13-foorOrderAppHomepageAllThingsLearnedSoFar/01-starting-project (10)/01-starting-project/src/components/UI/Modal.jsx
+++ 13-foorOrderAppHomepageAllThingsLearnedSoFar/01-starting-project (10)/01-starting-project/src/components/UI/Modal.jsx
@@ -7,6 +7,18 @@ import { createPortal } from 'react-dom';
 
 export default function Modal({children , open , className = ""}) {
     
+    /*
+    In this React code, the cleanup function inside useEffect will run in the following scenarios:
+
+    When the open dependency changes: Each time the open value changes, 
+    React will rerun the useEffect. Before re-executing, it will first 
+    run the cleanup function from the previous execution to ensure there’s no lingering state.
+
+    When the component unmounts: If the component that uses this useEffect 
+    hook is removed from the DOM, React will run the cleanup function to tidy up 
+    any resources or effects associated with the component.
+    
+    */
     const dialog = useRef()
 
     useEffect(() => {
